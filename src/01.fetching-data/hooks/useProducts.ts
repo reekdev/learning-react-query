fde9fc0847f7d897e975765a031ed2c2cdfca0d5
Reekdev Ray <ray.reekdev@gmail.com>
2024-01-06 22:25:31 +0530

    Initial commit Added code for fetching data with React query

---
+++ src/01.fetching-data/hooks/useProducts.ts
@@ -0,0 +1,35 @@
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+
+type Product = {
+  id: number;
+  title: string;
+  description: string;
+  price: number;
+  discountPercentage: number;
+  rating: number;
+  stock: number;
+  brand: string;
+  category: string;
+  thumbnail: string;
+  images: string[];
+};
+
+type ProductResponse = {
+  products: Product[];
+  total: number;
+  skip: number;
+  limit: number;
+};
+
+const useProducts = () => {
+  return useQuery<ProductResponse, Error>({
+    queryKey: ["products"],
+    queryFn: () =>
+      axios
+        .get<ProductResponse>("https://dummyjson.com/products")
+        .then((res) => res.data),
+  });
+};
+
+export default useProducts;
---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import FetchData from "./01.fetching-data/components/FetchData";
+
+function App() {
+  return (
+    <>
+      <FetchData />
+    </>
+  );
+}
+
+export default App;
